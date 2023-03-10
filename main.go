b9a4f56ab851ce8cbaea9db96a63fb9363ac8061
dror ivry <drorivry@gmail.com>
2023-03-10 20:43:22 +0200

    init the mod annd started the k8s client

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	batchv1 "k8s.io/api/batch/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	kubernetes "k8s.io/client-go/kubernetes"
+	clientcmd "k8s.io/client-go/tools/clientcmd"
+)
+
+func connectToK8s() *kubernetes.Clientset {
+	home, exists := os.LookupEnv("HOME")
+	if !exists {
+		home = "/root"
+	}
+
+	configPath := filepath.Join(home, ".kube", "config")
+
+	config, err := clientcmd.BuildConfigFromFlags("", configPath)
+	if err != nil {
+		log.Fatalln("failed to create K8s config")
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatalln("Failed to create K8s clientset")
+	}
+
+	return clientset
+}
+
+func launchK8sJob(clientset *kubernetes.Clientset, jobName *string, image *string, cmd *string) {
+	jobs := clientset.BatchV1().Jobs("default")
+	var backOffLimit int32 = 0
+
+	jobSpec := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      *jobName,
+			Namespace: "default",
+		},
+		Spec: batchv1.JobSpec{
+			Template: v1.PodTemplateSpec{
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name:    *jobName,
+							Image:   *image,
+							Command: strings.Split(*cmd, " "),
+						},
+					},
+					RestartPolicy: v1.RestartPolicyNever,
+				},
+			},
+			BackoffLimit: &backOffLimit,
+		},
+	}
+
+	_, err := jobs.Create(context.TODO(), jobSpec, metav1.CreateOptions{})
+	if err != nil {
+		log.Fatalln("Failed to create K8s job.")
+	}
+
+	//print job details
+	log.Println("Created K8s job successfully")
+}
+
+func main() {
+	jobName := flag.String("jobname", "test-job", "The name of the job")
+	containerImage := flag.String("image", "ubuntu:latest", "Name of the container image")
+	entryCommand := flag.String("command", "ls", "The command to run inside the container")
+
+	flag.Parse()
+
+	clientset := connectToK8s()
+	launchK8sJob(clientset, jobName, containerImage, entryCommand)
+}
