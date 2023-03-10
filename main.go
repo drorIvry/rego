aadc6ce1fffd3d5a3466fef42707beea63f08355
dror ivry <drorivry@gmail.com>
2023-03-10 22:59:32 +0200

    added waitgroop and scaffolded the tasker api

--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"flag"
+	"sync"
 
 	k8s_client "github.com/drorivry/matter/k8s"
+	"github.com/drorivry/matter/tasker"
+	"github.com/gin-gonic/gin"
 )
+
+func runServer(server *gin.Engine) {
+	server.Run()
+}
 
 func main() {
 	jobName := flag.String("jobname", "test-job", "The name of the job")
@@ -13,8 +20,15 @@ func main() {
 	namespace := flag.String("namespace", "default", "The job's namespace to deploy to")
 	kuneConfigPath := flag.String("kubeConfigPath", "", "The path to the kubeconfig")
 
+	var wg sync.WaitGroup
+
 	flag.Parse()
+
+	server := tasker.GetServer()
+	wg.Add(1)
+	go runServer(server)
 
 	clientset := k8s_client.ConnectToK8s(kuneConfigPath)
 	k8s_client.LaunchK8sJob(clientset, jobName, containerImage, entryCommand, namespace)
+	wg.Wait()
 }
--- tasker/tasker.go
+++ tasker/tasker.go
@@ -1,1 +1,17 @@
 package tasker
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetServer() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	return r
+}
