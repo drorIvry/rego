4e7b27529309bd047fbdc771653e0a47352899f7
dror ivry <drorivry@gmail.com>
2023-03-26 18:07:29 +0300

    reordered the api

--- controllers/task_definitions_controller.go
+++ controllers/task_definitions_controller.go
@@ -12,7 +12,14 @@ import (
 	"github.com/google/uuid"
 )
 
-
+// CreateTaskDefinition           godoc
+// @Summary      				  Create a new task definition
+// @Description                   Generate a new definition of a task to run with cadence, parameters and runtime data
+// @Tags                          definition
+// @Produce                       application/json
+// @Param                         newTaskDef  body models.TaskDefinition  true  "Task definition JSON"
+// @Success                       200
+// @Router                        /task [post]
 func CreateTaskDefinition(c *gin.Context) {
 	var newTaskDef models.TaskDefinition
 
@@ -39,16 +46,38 @@ func CreateTaskDefinition(c *gin.Context) {
 	)
 }
 
+// GetAllTaskDefinitions          godoc
+// @Summary      				  Get all task definitions
+// @Description                   Filter to get the task definitions you need
+// @Tags                          definition
+// @Produce                       json
+// @Success                       200 {object} []models.TaskDefinition
+// @Router                        /task [get]
 func GetAllTaskDefinitions(c *gin.Context) {
 	tasks := dao.GetAllTaskDefinitions()
 	c.IndentedJSON(http.StatusOK, tasks)
 }
 
+// GetAllPendingTaskDefinitions   godoc
+// @Summary      				  Get all of the pending task definitions
+// @Description                   Filter to get the task pending tasks
+// @Tags                          definition
+// @Produce                       json
+// @Success                       200 {object} []models.TaskDefinition
+// @Router                        /task/pending [get]
 func GetAllPendingTaskDefinitions(c *gin.Context) {
 	tasks := dao.GetPendingTasks()
 	c.IndentedJSON(http.StatusOK, tasks)
 }
 
+// AbortTaskExecution             godoc
+// @Summary      				  Rerun a task definition
+// @Description                   Rerun a task definition previously created
+// @Tags                          definition
+// @Produce                       json
+// @Param                         definitionId  path string  true  "The task definition id"
+// @Success                       200
+// @Router                        /task/{definitionId}/rerun [get]
 func RerunTask(c *gin.Context) {
 	uuidParam := c.Param("definitionId")
 	var definitionId, err = uuid.Parse(uuidParam)
@@ -72,14 +101,38 @@ func RerunTask(c *gin.Context) {
 	})
 }
 
+// GetLatestExecution	          godoc
+// @Summary      				  Get the latest execution of a given definitions
+// @Description                   Filter to get the task definitions you need
+// @Tags                          definition
+// @Produce                       json
+// @Success                       200 {object} []models.TaskExecution
+// @Param                         definitionId  path string  true  "The task definition id"
+// @Router                        /task/{definitionId}/latest [get]
 func GetLatestExecution(c *gin.Context) {
 
 }
 
+// UpdateTaskDefinition           godoc
+// @Summary      				  Update a task definition
+// @Description                   Update a definition of a task to run with cadence, parameters and runtime data
+// @Tags                          definition
+// @Produce                       application/json
+// @Param                         newTaskDef  body models.TaskDefinition  true  "Task definition JSON"
+// @Success                       200
+// @Router                        /task [put]
 func UpdateTaskDefinition(c *gin.Context) {
 
 }
 
+// DeleteTaskDefinition           godoc
+// @Summary      				  Delete a task definition
+// @Description                   Mark a task definition as deleted (it is not actually deleted from the db)
+// @Tags                          definition
+// @Produce                       application/json
+// @Param                         definitionId  path string  true  "The task definition id"
+// @Success                       200
+// @Router                        /task/{definitionId} [delete]
 func DeleteTaskDefinition(c *gin.Context) {
 
 }
