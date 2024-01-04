5de1f182996e3307fb7225da5dca07b913b20e96
Yuval Siev <yuvall31@gmail.com>
2024-01-04 08:47:22 +0200

    Add zerolog to gin server as well

--- tasker/tasker.go
+++ tasker/tasker.go
@@ -6,7 +6,9 @@ import (
 
 	"github.com/drorivry/rego/controllers"
 	_ "github.com/drorivry/rego/swagger-docs"
+	"github.com/rs/zerolog"
 
+	"github.com/gin-contrib/logger"
 	"github.com/gin-gonic/gin"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -17,20 +19,26 @@ func GetServer(port int) *http.Server {
 	r.GET("/ping", controllers.Ping)
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
+	ginZerologger := logger.SetLogger(
+		logger.WithLogger(func(_ *gin.Context, l zerolog.Logger) zerolog.Logger {
+			return l.Output(gin.DefaultWriter).With().Logger()
+		}),
+	)
+
 	v1 := r.Group("/api/v1")
 	{
 		// Definitions
-		v1.GET("/task", controllers.GetAllTaskDefinitions)
-		v1.POST("/task", controllers.CreateTaskDefinition)
-		v1.POST("/task/:definitionId/rerun", controllers.RerunTask)
-		v1.GET("/task/:definitionId/latest", controllers.GetLatestExecution)
-		v1.PUT("/task", controllers.UpdateTaskDefinition)
-		v1.DELETE("/task/:definitionId", controllers.DeleteTaskDefinition)
-		v1.GET("/tasks/pending", controllers.GetAllPendingTaskDefinitions)
+		v1.GET("/task", ginZerologger, controllers.GetAllTaskDefinitions)
+		v1.POST("/task", ginZerologger, controllers.CreateTaskDefinition)
+		v1.POST("/task/:definitionId/rerun", ginZerologger, controllers.RerunTask)
+		v1.GET("/task/:definitionId/latest", ginZerologger, controllers.GetLatestExecution)
+		v1.PUT("/task", ginZerologger, controllers.UpdateTaskDefinition)
+		v1.DELETE("/task/:definitionId", ginZerologger, controllers.DeleteTaskDefinition)
+		v1.GET("/tasks/pending", ginZerologger, controllers.GetAllPendingTaskDefinitions)
 
 		// Executions
-		v1.GET("/execution", controllers.RerunTask)
-		v1.POST("/execution/:executionId/abort", controllers.AbortTaskExecution)
+		v1.GET("/execution", ginZerologger, controllers.RerunTask)
+		v1.POST("/execution/:executionId/abort", ginZerologger, controllers.AbortTaskExecution)
 	}
 
 	server := &http.Server{
