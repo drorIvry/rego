324bc0f3c9c006e443a2c1c120c5625b5b17f586
Yuval Siev <yuvall31@gmail.com>
2024-01-20 11:20:51 +0200

    use env variables better

--- ui/src/server/auth.ts
+++ ui/src/server/auth.ts
@@ -7,7 +7,7 @@ import type { Adapter } from "next-auth/adapters";
 import EmailProvider from "next-auth/providers/email";
 import DiscordProvider from "next-auth/providers/discord";
 import GitHubProvider from "next-auth/providers/github";
-
+import { env } from "../env.js";
 import { db } from "~/server/db";
 import { sqlDrizzleAdapter } from "./db/auth-adapter";
 import type { NeonDatabase } from "drizzle-orm/neon-serverless";
@@ -54,24 +54,24 @@ export const authOptions: NextAuthOptions = {
   adapter: sqlDrizzleAdapter(db as unknown as NeonDatabase) as Adapter,
   providers: [
     DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID ?? "",
-      clientSecret: process.env.DISCORD_CLIENT_SECRET ?? "",
+      clientId: env.DISCORD_CLIENT_ID ?? "",
+      clientSecret: env.DISCORD_CLIENT_SECRET ?? "",
     }),
     GitHubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
+      clientId: env.GITHUB_ID ?? "",
+      clientSecret: env.GITHUB_SECRET ?? "",
     }),
-    EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
-      from: process.env.EMAIL_FROM,
-    }),
+    // EmailProvider({
+    //   server: {
+    //     host: process.env.EMAIL_SERVER_HOST,
+    //     port: process.env.EMAIL_SERVER_PORT,
+    //     auth: {
+    //       user: process.env.EMAIL_SERVER_USER,
+    //       pass: process.env.EMAIL_SERVER_PASSWORD,
+    //     },
+    //   },
+    //   from: process.env.EMAIL_FROM,
+    // }),
   ],
 };
 
